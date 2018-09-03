cc24e17d97f777cef1cf670297089331fc4bb3b6
Danny Raufeisen <danny.raufeisen@jtl-software.com>
2018-09-03 12:19:09 +0200

    Clear

--- scripts/pack-atlases.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import os
-
-for d in os.listdir("gfxsets"):
-	res = os.system("./libs/spritegl/scripts/pack-images.py gfx/" + d + ".png gfxsets/" + d + "/*")
-	if res != 0:
-		exit()
-
